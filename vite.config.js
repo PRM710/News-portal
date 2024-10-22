7c4fa7d6d06508a1120764b2d274e5fa9821bc16
PRM710 <99472825+PRM710@users.noreply.github.com>
2024-10-22 13:21:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',      // Bind to all available network interfaces
-    port: process.env.PORT || 3000,  // Use the port provided by Render, or default to 3000
-  }
-})
+    port: process.env.PORT || 3000,  // Use the port provided by Render or default to 3000
+  },
+  build: {
+    outDir: 'dist',       // Output directory for build files
+  },
+});
